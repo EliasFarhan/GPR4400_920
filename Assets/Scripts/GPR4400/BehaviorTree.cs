565a9d164a06ee10e41994bedded9df435e41fc4
Elias Farhan <elias.farhan@gmail.com>
2021-04-22 22:56:36 +0200

    Adding some tests for behvaior tree, adding exception to calling a finished node and remove error status

--- Assets/Scripts/GPR4400/BehaviorTree.cs
+++ Assets/Scripts/GPR4400/BehaviorTree.cs
@@ -1,5 +1,7 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
+using UnityEditor.Build.Reporting;
 using UnityEngine;
 using UnityEngine.Assertions.Must;
 using UnityEngine.PlayerLoop;
@@ -11,8 +13,14 @@ namespace bt
         Init,
         Success,
         Failure,
-        Running,
-        Error
+        Running
+    }
+
+    public class BehaviorException : Exception
+    {
+        public BehaviorException(string msg) : base(msg)
+        { 
+        }
     }
     public abstract class Behavior
     {
@@ -25,9 +33,15 @@ namespace bt
 
         protected void InitIfNeeded()
         {
-            if (Status == Status.Init)
+            switch (Status)
             {
-                Init();
+                case Status.Init:
+                    Init();
+                    break;
+                case Status.Failure:
+                case Status.Success:
+                    throw new BehaviorException(
+                        "Do not call finished behavior");
             }
         }
         /**
@@ -62,9 +76,11 @@ namespace bt
             switch (status)
             {
                 case Status.Failure:
-                    return Status.Success;
+                    Status = Status.Success;
+                    return Status;
                 case Status.Success:
-                    return Status.Failure;
+                    Status = Status.Failure;
+                    return Status;
             }
             return status;
         }
@@ -78,11 +94,11 @@ namespace bt
             var status = child_.Update();
             switch (status)
             {
-                case Status.Error:
                 case Status.Running:
                     return status;
             }
-            return Status.Success;
+            Status = Status.Success;
+            return Status;
         }
     }
 
@@ -102,11 +118,34 @@ namespace bt
             return status;
         }
     }
+
+    public class RepeatUntilFail : Decorator
+    {
+        public override Status Update()
+        {
+            InitIfNeeded();
+            var status = child_.Update();
+            switch (status)
+            {
+                case Status.Failure:
+                    Status = Status.Success;
+                    return Status;
+                case Status.Success:
+                    Status = Status.Init;
+                    break;
+            }
+            return status;
+        }
+    }
     
     public abstract class Composite : Behavior
     {
         protected Behavior[] children_;
-        public Behavior[] Children => children_;
+        public Behavior[] Children
+        {
+            get => children_;
+            set => children_ = value;
+        }
 
         public override void Init()
         {
@@ -125,12 +164,18 @@ namespace bt
             InitIfNeeded();
             foreach (var child in children_)
             {
+                switch (child.Status)
+                {
+                    case Status.Success:
+                        continue;
+                }
                 var status = child.Update();
                 switch (status)
                 {
                     case Status.Running:
-                    case Status.Error:
+                        return Status.Running;
                     case Status.Failure:
+                        Status = status;
                         return status;
                 }
             }
@@ -146,12 +191,18 @@ namespace bt
             InitIfNeeded();
             foreach (var child in children_)
             {
+                switch (child.Status)
+                {
+                    case Status.Failure:
+                        continue;
+                }
                 var status = child.Update();
                 switch (status)
                 {
                     case Status.Running:
-                    case Status.Error:
+                        return Status.Running;
                     case Status.Success:
+                        Status = status;
                         return status;
                 }
             }
--- Assets/Scripts/Tests/BehaviorTreeTest.cs
+++ Assets/Scripts/Tests/BehaviorTreeTest.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using bt;
@@ -44,6 +45,23 @@ namespace bt
             return Status;
         }
     }
+
+    class InitCounterBehavior : Behavior
+    {
+        private int counter_ = 0;
+        public int Counter => counter_;
+        public override void Init()
+        {
+            base.Init();
+            counter_++;
+        }
+
+        public override Status Update()
+        {
+            InitIfNeeded();
+            return Status.Success;
+        }
+    }
 }
 
 public class BehaviorTreeTest
@@ -54,13 +72,38 @@ public class BehaviorTreeTest
         var inverter1 = new Inverter {Child = new SuccessBehavior()};
 
         Assert.AreEqual(Status.Failure, inverter1.Update());
-
+        try
+        {
+            inverter1.Update();
+            Assert.Fail("Calling decorator when success should throw!");
+        }
+        catch (BehaviorException e)
+        {
+            Assert.Pass("Calling decorator when success should throw!");
+        }
         var inverter2 = new Inverter {Child = new FailureBehavior()};
         
         Assert.AreEqual(Status.Success, inverter2.Update());
-
+        try
+        {
+            inverter2.Update();
+            Assert.Fail("Calling decorator when success should throw!");
+        }
+        catch (BehaviorException e)
+        {
+            Assert.Pass("Calling decorator when success should throw!");
+        }
         var inverter3 = new Inverter {Child = new RunningBehavior()};
-        Assert.AreEqual(Status.Running, inverter3.Update());
+        Assert.AreEqual(Status.Running, inverter3.Update());        
+        try
+        {
+            inverter3.Update();
+            Assert.Pass("Calling decorator while running should not throw!");
+        }
+        catch (BehaviorException e)
+        {
+            Assert.Fail("Calling decorator while running should not throw!");
+        }
     }
 
     [Test]
@@ -69,13 +112,96 @@ public class BehaviorTreeTest
         var succeeder1 = new Succeeder {Child = new SuccessBehavior()};
 
         Assert.AreEqual(Status.Success, succeeder1.Update());
-
+        try
+        {
+            succeeder1.Update();
+            Assert.Fail("Calling decorator when success should throw!");
+        }
+        catch (BehaviorException e)
+        {
+            Assert.Pass("Calling decorator when success should throw!");
+        }
         var succeeder2 = new Succeeder {Child = new FailureBehavior()};
         
         Assert.AreEqual(Status.Success, succeeder2.Update());
-
+        try
+        {
+            succeeder2.Update();
+            Assert.Fail("Calling decorator when success should throw!");
+        }
+        catch (BehaviorException e)
+        {
+            Assert.Pass("Calling decorator when success should throw!");
+        }
         var succeeder3 = new Succeeder {Child = new RunningBehavior()};
         Assert.AreEqual(Status.Running, succeeder3.Update());
+        try
+        {
+            succeeder3.Update();
+            Assert.Pass("Calling decorator while running should not throw!");
+        }
+        catch (BehaviorException e)
+        {
+            Assert.Fail("Calling decorator while running should not throw!");
+        }
+    }
+
+    [Test]
+    public void RepeaterTest()
+    {
+        var counter = new InitCounterBehavior();
+        var repeater1 = new Repeater {Child = counter};
+        repeater1.Update();
+        repeater1.Update();
+        repeater1.Update();
+        Assert.AreEqual(counter.Counter, 3);
+    }
+
+    [Test]
+    public void SequenceTest()
+    {
+        var sequence1 = new Sequence
+        {
+            Children = new Behavior[]
+            {
+                new SuccessBehavior(), new SuccessBehavior()
+            }
+        };
+        Assert.AreEqual(Status.Success, sequence1.Update());
+        try
+        {
+            sequence1.Update();
+        }
+        catch (BehaviorException e)
+        {
+            Assert.Pass("Calling sequence when succeded should throw");
+        }
+        var sequence2 = new Sequence
+        {
+            Children = new Behavior[]
+            {
+                new SuccessBehavior(), new RunningBehavior()
+            }
+        };
+        Assert.AreEqual(Status.Running, sequence2.Update());
+        Assert.AreEqual(Status.Running, sequence2.Update());
+        
+        var sequence3 = new Sequence
+        {
+            Children = new Behavior[]
+            {
+                new SuccessBehavior(), new FailureBehavior()
+            }
+        };
+        Assert.AreEqual(Status.Failure, sequence3.Update());
+        try
+        {
+            sequence3.Update();
+        }
+        catch (BehaviorException e)
+        {
+            Assert.Pass("Calling sequence when failed should throw");
+        }
     }
 
 }
